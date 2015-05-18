273eb41dd49a970e7261e7355a93e2fb0f32068e
praba1110 <praba1110@gmail.com>
2015-05-18 20:06:08 +0530

    First commit

---
+++ Task1_deltaappdev/app/src/main/java/com/example/praba1110/task1_deltaappdev/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.praba1110.task1_deltaappdev;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+    int counter=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void buttonclick(View view)
+    {
+        counter++;
+        TextView text=(TextView)findViewById(R.id.text);
+        text.setText("Button clicked  "+counter+"  times");
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
